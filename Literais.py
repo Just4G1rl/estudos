c9c37573a2bef9e6d0e38d8ec76f71d6c39063db
Barbara <barbara.g.n.silva@gmail.com>
2025-02-02 19:45:52 -0300

    Quinta alteração

---
+++ Literais.py
@@ -0,0 +1,8 @@
+print("2")
+print(2)
+print(11111111)
+print(11_111_111) #outra forma aceita de escrevere números
+print(0O1542) #Conversão octal para decimal
+print(0x2b58) #Conversão hexadecimal para decimal
+print(0.8) #valores decimais são chamados de float
+print(3e8) #a letra "e" é usada para representar potências
